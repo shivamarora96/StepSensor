b520a1fc0258fb131a39a5b9ab6ba1401535bc8f
Shivam Arora <shivam_arora@live.in>
2016-11-30 00:11:26 +0530

    commit-Opening Screen

--- app/src/main/java/com/example/shivamarora/stepsensor/OpeningScreen.java
+++ app/src/main/java/com/example/shivamarora/stepsensor/OpeningScreen.java
@@ -1,34 +1,53 @@
+
 package com.example.shivamarora.stepsensor;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
+import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.util.Log;
 
 public class OpeningScreen extends AppCompatActivity {
-
-    AlarmManager alarmManager ;
-    ImageView i ;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_screen);
 
-
-        int delay = 4 ;
-        Intent [] intent = new Intent[1] ;
-        intent[0] = new Intent(this , One.class) ;
-PendingIntent pendingIntent  = PendingIntent.getActivities(OpeningScreen.this , 007 , intent , 0) ;
-        overridePendingTransition(android.R.anim.overshoot_interpolator , android.R.anim.overshoot_interpolator);
-
-        alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE) ;
-        alarmManager.set(AlarmManager.RTC, System.currentTimeMillis() + delay * 1000, pendingIntent );
+       DoinBackground d =  new DoinBackground(this) ;
+         d.execute();
 
 
+    }
+}
 
+class DoinBackground extends AsyncTask<Void , Void , Void>{
+
+    OpeningScreen openingScreen ;
+
+    public DoinBackground(OpeningScreen openingScreen) {
+        this.openingScreen = openingScreen;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Log.i("thread", "error") ;
+        }
+return null ;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+
+        Intent i = new Intent(openingScreen , GetStartedScreen.class) ;
+        openingScreen.startActivity(i);
+        openingScreen.finish();
+
+        super.onPostExecute(aVoid);
     }
 }
